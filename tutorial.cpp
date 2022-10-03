e94430b78c0c18eb73c3b4b8f1fc34a7e1bb331e
Bacham Venkat <bachamvenkat@icloud.com>
2022-10-03 12:54:14 +0530

    Update tutorial.cpp
    Added Insertion sort Algo

--- tutorial.cpp
+++ tutorial.cpp
@@ -166,3 +166,21 @@ void quickSort(int array[], int low, int high) {
         }     
     }     
  }  
+
+
+// Insertion Sort
+void insert(int a[], int n) 
+{  
+    int i, j, temp;  
+    for (i = 1; i < n; i++) {  
+        temp = a[i];  
+        j = i - 1;  
+  
+        while(j>=0 && temp <= a[j])  /* Move the elements greater than temp to one position ahead from their current position*/  
+        {    
+            a[j+1] = a[j];     
+            j = j-1;    
+        }    
+        a[j+1] = temp;    
+    }  
+}  
