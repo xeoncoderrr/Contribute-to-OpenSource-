ff7b6795e6988f1de806e850a07c2ebdf1436bb2
ashutosh sao <63497482+ashutoshsao@users.noreply.github.com>
2022-10-04 10:17:33 +0530

    added for loop
    this code show simply how to use for loop to print table of 3

---
+++ forloop.cpp
@@ -0,0 +1,14 @@
+###A simple code example to show how to use while loop
+
+//in this example we are printing a table of 3
+
+#include<iostream>
+using namespace std;
+
+int main(){
+     int n=3;
+     for(int i=1;i<=10;i++){
+            cout<<i<<"*"<<n<<"="<<i*n<<endl;
+     }
+    return 0;
+}
