fcc5bc95e701f49bee44340715d6613b130456a0
ashutosh sao <63497482+ashutoshsao@users.noreply.github.com>
2022-10-04 10:15:47 +0530

    added while loop
    code shows simple implementation of while loop to print table of 4

---
+++ whileloop.cpp
@@ -0,0 +1,15 @@
+###A simple code example to show how to use while loop
+
+//in this example we are printing a table of 4
+
+#include<iostream>
+using namespace std;
+
+int main(){
+     int i=1,n=4;
+     while(i<=10){
+            cout<<i<<"*"<<n<<"="<<i*n<<endl;
+            i++;
+     }
+    return 0;
+}
